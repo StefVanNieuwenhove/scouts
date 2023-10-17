d386f02bf9639ab55781facb619373a3d9402ef3
stefvannieuwenhove <stef.vannieuwenhove@telenet.be>
2023-10-17 23:49:07 +0200

    get all users

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 
 const router = express.Router();
 
-const usersRouter = require('./users');
+import usersRouter from './users';
 
 router.use('/user', usersRouter);
 
