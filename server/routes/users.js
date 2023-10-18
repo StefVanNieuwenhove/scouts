9ad3f18efb5310cf4746b0e44fdd1b0be09b0d0f
stefvannieuwenhove <stef.vannieuwenhove@telenet.be>
2023-10-18 09:54:29 +0200

    user/:id route

--- server/routes/users.js
+++ server/routes/users.js
@@ -13,4 +13,15 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/:id', async (req, res) => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id: req.params.id },
+    });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
+
 module.exports = router;
