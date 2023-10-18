4eefd42ff6bcc80e9fd87277f7c08aca1b783384
stefvannieuwenhove <stef.vannieuwenhove@telenet.be>
2023-10-18 16:22:39 +0200

    update cors options

--- server/app.js
+++ server/app.js
@@ -13,6 +13,9 @@ const PORT = process.env.PORT || 3000;
 const corsOptions = {
   origin: ['http://localhost:3000', 'http://localhost:5137'],
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  optiosSuccessStatus: 200,
 };
 
 // Middleware
