46ca5f80048ba1be5598bb96a3f8565432251236
stefvannieuwenhove <stef.vannieuwenhove@telenet.be>
2023-10-18 17:17:00 +0200

    add origin cors

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
-  origin: 'http://localhost:5173', // Replace with your client's actual origin
+  origin: ['http://localhost:5173', 'http://localhost:4173'], // Replace with your client's actual origin
   credentials: true, // Allow cookies to be sent with the request
 };
 
