266f504f07e5ba2bb8131fcf6dd24a4451c8e79a
stefvannieuwenhove <stef.vannieuwenhove@telenet.be>
2024-01-05 12:33:11 +0100

    add: default settings for fetch the data

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,7 +6,15 @@ import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider, ThemeProvider } from './context';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-const client = new QueryClient();
+const client = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      staleTime: 1000 * 60 * 60 * 1, // 1 hour
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
