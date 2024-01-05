8286ca4e03e9276e7ce796e1ef973e0379406b3d
stefvannieuwenhove <stef.vannieuwenhove@telenet.be>
2024-01-05 16:48:12 +0100

    Refactor encryption and decryption functions

--- server/middleware/hash.ts
+++ server/middleware/hash.ts
@@ -1,79 +1,10 @@
 import crypto, { CipherKey, BinaryLike, CipherCCM } from 'crypto';
-/*
-// Environment variables for secrets and algorithm
-const HASH_SECRET = process.env.HASH_SECRET || 'secret';
-const HASH_ALGORITHM = process.env.HASH_ALGORITHM || 'aes-256-gcm';
-const IV_SECRET = process.env.IV_SECRET || 'secret';
 
-// Create a key for AES-256-CCM
-const key: CipherKey = crypto
-  .createHash('sha512')
-  .update(HASH_SECRET)
-  .digest('hex')
-  .substring(0, 32);
-
-// Create an initialization vector (IV)
-const encryptionIV: BinaryLike = crypto
-  .createHash('sha512')
-  .update(IV_SECRET)
-  .digest('hex')
-  .substring(0, 12);
-
-// Encryption function
-const encrypt = (text: string): { encryptedData: string; authTag: Buffer } => {
-  if (!HASH_SECRET || !IV_SECRET || !HASH_ALGORITHM) {
-    throw new Error('Missing environment variables');
-  }
-
-  const cipher = crypto.createCipheriv(
-    HASH_ALGORITHM,
-    key,
-    encryptionIV
-  ) as CipherCCM;
-
-  let encrypted = cipher.update(text, 'utf8', 'hex');
-  encrypted += cipher.final('hex');
-
-  return {
-    encryptedData: Buffer.from(encrypted, 'hex').toString('base64'),
-    authTag: cipher.getAuthTag(),
-  };
-};
-
-// Decryption function
-const decrypt = (encryptedData: string, authTag: Buffer): string => {
-  if (!HASH_SECRET || !IV_SECRET || !HASH_ALGORITHM) {
-    throw new Error('Missing environment variables');
-  }
-
-  console.log({ encryptedData, authTag });
-
-  const decipher = crypto.createDecipheriv(
-    HASH_ALGORITHM,
-    key,
-    encryptionIV
-  ) as CipherCCM;
-
-  //decipher.setAAD(authTag, { plaintextLength: encryptedData.length });
-  decipher.getAuthTag();
-
-  const buff = Buffer.from(encryptedData, 'base64');
-  let decrypted = decipher.update(buff.toString('hex'), 'hex', 'utf8');
-  decrypted += decipher.final('utf8');
-
-  return decrypted;
-};
-
-export { encrypt, decrypt };
- */
-
-const SECRET_KEY: string = 'secret';
-
-interface EncryptedData {
+type EncryptedData = {
   iv: Buffer;
   data: string;
   secret_key: Buffer;
-}
+};
 
 function encrypt(data: string): EncryptedData {
   const secret_key = crypto.randomBytes(32);
