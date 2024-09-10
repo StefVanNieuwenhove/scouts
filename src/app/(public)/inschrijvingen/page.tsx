4f738a70aea2d05a0ebad905febe6d6b84afe53f
Stef Van Nieuwenhove <stef.vannieuwenhove@student.hogent.be>
2024-09-10 12:00:12 +0200

    created inschrijvingen page

---
+++ src/app/(public)/inschrijvingen/page.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { H3 } from '@/components/typography';
+import React from 'react';
+
+const InschrijvingenPage = () => {
+  return (
+    <main className='container mx-auto h-screen w-full flex items-center justify-center mb-10'>
+      <H3>De inschrijvingen zijn gesloten</H3>
+      {/* <iframe
+          width='100%'
+          height='100%'
+          src='https://groepsadmin.scoutsengidsenvlaanderen.be/groepsadmin/lidworden?groep=O2320G'
+          title='Inschrijvingen'
+          allowFullScreen
+        /> */}
+    </main>
+  );
+};
+
+export default InschrijvingenPage;
